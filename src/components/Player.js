a4f221da19278fa38e8b243482835ea6b81de0f3
reinoptland <reinoptland@gmail.com>
2019-09-10 16:02:47 +0200

    Using callback props to update player scores!

---
+++ src/components/Player.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Player extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired
+  }
+
+  handleClick = () => {
+      // use callback prop to call back to the parent!
+      this.props.incrementScore(this.props.id)
+  }
+
+  render() {
+    return (
+      <li className="player">
+        <p className="name">{ this.props.name }</p>
+        <p className="score">{ this.props.score }</p>
+        <button onClick={this.handleClick} >+</button>
+      </li>
+    )
+  }
+}
---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import Player from "./Player";
+
+export default class Scoreboard extends Component {
+    // we want to change the score of a player
+    // we want to order players by how many points they have
+    // we want to add players
+
+    state = {
+        players: [
+            { id: 1, name: "Arien", score: 2 },
+            { id: 2, name: "David", score: 5 },
+            { id: 3, name: "Mimi",  score: 4 },
+            { id: 4, name: "Rein",  score: 4 },
+        ]
+    }
+
+    // define callback in scoreboard so we can use this.setState later
+    incrementScore = (id) => {
+        // console.log to check if we have the id
+        console.log('call me maybe', id)
+
+        // map over the players and change the score of the player
+        // who matches the id
+        const newplayers = this.state.players.map(player => {
+            if(player.id === id){
+                // change score by mutation :(
+                // player.score = player.score + 1
+                // return player
+
+                // change score by return a new object 
+                // no mutation :) 
+                // { ...spreadObject, keyIWantToChange: value }
+                return { ...player, score: player.score + 1 }
+            }
+
+            return player
+        })
+
+        // update the state with the new array
+        this.setState({ player: newplayers })
+    }
+
+    render() {
+        return (
+            <div className="scoreboard">
+                <h1>Scoreboard</h1>
+                <ul>
+                    { this.state.players.map(player => {
+                        return <Player 
+                            key={player.id}
+                            name={player.name} 
+                            score={player.score} 
+                            id={player.id}
+                            // pass down out callback to the childe
+                            // together with the other props
+                            incrementScore={this.incrementScore}
+                        />
+                    }) }
+                </ul>
+            </div>
+        )
+    }
+}
